01241c28aad41f45ab29f84f648707f6dff04bb3
(하동호) <(cubix101@naver.com)>
2021-02-14 13:21:08 +0900

    로그인 모달창

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch ,Route} from 'react-router-dom';
 import Home from "./routes/Home";
 import Registration from "./routes/Registration";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
