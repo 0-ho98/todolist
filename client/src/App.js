93aa13c620b82c17e4ebc7d76e62a9556b7faf00
(하동호) <(cubix101@naver.com)>
2021-02-14 13:41:11 +0900

    커밋이요

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,8 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 // import Navigation from './components/Navigation';
 import Home from "./routes/Home";
-<<<<<<< HEAD
 import Registration from "./routes/Registration";
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
->>>>>>> f80cc4ec82eb1dff6874899ed26801f3005d846e
 function App() {
   return (
     <BrowserRouter>
